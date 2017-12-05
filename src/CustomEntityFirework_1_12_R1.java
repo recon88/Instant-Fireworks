1a000b9a811bb9de0621a5c2738500c632a2f17e
recon88 <info@ingoguther.de>
2017-12-05 13:49:35 +0100

    Updated to 1.12-R1

---
+++ src/CustomEntityFirework_1_12_R1.java
@@ -0,0 +1,58 @@
+import org.bukkit.FireworkEffect;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.FireworkMeta;
+
+import net.minecraft.server.v1_12_R1.EntityFireworks;
+import net.minecraft.server.v1_12_R1.PacketPlayOutEntityStatus;
+import net.minecraft.server.v1_12_R1.World;
+
+public class CustomEntityFirework_1_12_R1 extends EntityFireworks {
+	Player[] players = null;
+
+	public CustomEntityFirework_1_12_R1(World world, Player... p) {
+		super(world);
+		players = p;
+		this.a(0.25F, 0.25F);
+	}
+
+	boolean gone = false;
+
+	@Override
+	public void B_() {
+		if (gone) {
+			return;
+		}
+
+		if (!this.world.isClientSide) {
+			gone = true;
+
+			if (players != null)
+				if (players.length > 0)
+					for (Player player : players)
+						(((CraftPlayer) player).getHandle()).playerConnection.sendPacket(new PacketPlayOutEntityStatus(this, (byte) 17));
+				else
+					world.broadcastEntityEffect(this, (byte) 17);
+			this.die();
+		}
+	}
+
+	public static void spawn(Location location, FireworkEffect effect, Player... players) {
+		try {
+			CustomEntityFirework_1_12_R1 firework = new CustomEntityFirework_1_12_R1(((CraftWorld) location.getWorld()).getHandle(), players);
+			FireworkMeta meta = ((Firework) firework.getBukkitEntity()).getFireworkMeta();
+			meta.addEffect(effect);
+			((Firework) firework.getBukkitEntity()).setFireworkMeta(meta);
+			firework.setPosition(location.getX(), location.getY(), location.getZ());
+
+			if ((((CraftWorld) location.getWorld()).getHandle()).addEntity(firework)) {
+				firework.setInvisible(true);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
